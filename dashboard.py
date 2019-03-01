802403f239ac019122c8d55ee4855e9f6b182c93
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2019-03-01 13:02:02 +0200

    Initial serverless version

--- dashboard.py
+++ dashboard.py
@@ -3,11 +3,6 @@
 
 """
 A script for rendering AWS dashboards to be viewed without logging in.
-Supply AWS credentials via an assigned role in IAM or environment variables.
-
-Usage example: ./aws-dashboard.py all > foo.html
-
-Logger runtime output will go to stderr.
 """
 
 import base64
@@ -15,17 +10,27 @@ import datetime
 import json
 import logging
 import sys
+import os
 
 import boto3
 from botocore.exceptions import ClientError
 
-__author__ = "Markus Koskinen"
+__author__ = "Markus Koskinen&Jyrki Ahpola"
 __license__ = "BSD"
 
-_LOG_FORMAT = "%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s"
-_DEBUG = False
+www_bucket = os.getenv('www_bucket', 'jahp-foobar')
 
-logging.basicConfig(level=logging.INFO, format=_LOG_FORMAT)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def store_to_s3(data,www_bucket):
+    try:
+        s3 = boto3.resource('s3')
+        content = s3.Object(www_bucket,"index.html")
+        content.put(Body=data,ContentType='text/html')
+    except ClientError as e:
+        logging.error("Writing to S3 failed: %s", e)
+        raise Exception
 
 def syntax(execname):
     print("Syntax: %s [dashboard_name]" % execname)
@@ -37,8 +42,7 @@ def auth():
         api = boto3.client('cloudwatch')
     except ClientError as e:
         logging.error("Client error: %s", e)
-        exit(1)
-        return e
+        raise Exception
     else:
         return api
 
@@ -48,7 +52,7 @@ def get_dashboard_images(dashboard_name="all", encode_base64=True):
 
     dashboard_list = [d['DashboardName'] for d in cloudwatch_api.list_dashboards()['DashboardEntries']]
 
-    logging.info("Dashboards available: %s Rendering: %s", dashboard_list, dashboard_name)
+    logger.info("Dashboards available: %s Rendering: %s", dashboard_list, dashboard_name)
 
     if dashboard_name != "all" and dashboard_name in dashboard_list:
         dashboard_list = [dashboard_name]
@@ -56,14 +60,14 @@ def get_dashboard_images(dashboard_name="all", encode_base64=True):
     dashboard_widget_properties = {}
     dashboard_images = {}
 
-    logging.debug("Dashboards available: %s ", dashboard_list)
+    logger.debug("Dashboards available: %s ", dashboard_list)
 
     for dashboard in dashboard_list:
         dashboard_widget_properties[dashboard] = [dp['properties'] for dp in json.loads(cloudwatch_api.get_dashboard(DashboardName=dashboard)['DashboardBody'])['widgets'] if 'metrics' in dp['properties']]
         dashboard_images[dashboard] = []
 
         for dashboard_widget_property in dashboard_widget_properties[dashboard]:
-            #logging.debug(json.dumps(dashboard_widget_property))
+            logger.debug(json.dumps(dashboard_widget_property))
 
             dashboard_image = cloudwatch_api.get_metric_widget_image(MetricWidget=json.dumps(dashboard_widget_property))['MetricWidgetImage']
 
@@ -73,18 +77,13 @@ def get_dashboard_images(dashboard_name="all", encode_base64=True):
             dashboard_images[dashboard].append(dashboard_image)
 
     result = [item for sublist in dashboard_images.values() for item in sublist]
-    logging.info("Result size: %d ", len(result))
+    logger.info("Result size: %d ", len(result))
 
     return result
 
-
-
-def main(argv):
+def main(event, context):
     """ Outputs a HTML page to stdout with inline base64 encoded PNG dashboards. """
     dashboard_name = "all"
-
-    if len(argv) == 2:
-        dashboard_name = argv[1]
 
     result = "<html>\n"
 
@@ -97,13 +96,14 @@ def main(argv):
     end_time = datetime.datetime.now()
     result += "<!-- Generation ended: " + end_time.isoformat() + " -->\n"
     result += "</html>\n"
+    
+    logger.info("Completed. Start time: %s Runtime: %s ", start_time, str(end_time-start_time))
 
-    print("%s" % result)
-    logging.info("Completed. Start time: %s Runtime: %s ", start_time, str(end_time-start_time))
-
-    return 0
-
-if __name__ == "__main__":
-    if len(sys.argv) not in  (1, 2):
-        syntax(sys.argv[0])
-    sys.exit(main(sys.argv))
+    store_to_s3(result,www_bucket)
+    
+    response = {
+        "statusCode": 200,
+        "body": "Foo"
+    }
+    
+    return response
